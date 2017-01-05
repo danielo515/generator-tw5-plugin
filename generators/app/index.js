0bbff1318406f6ab57446777016f80b3bcb3ca56
Danielo Rodriguez Rivero <rdanielo@gmail.com>
2017-01-05 02:19:17 +0100

    First fully functional version

--- generators/app/index.js
+++ generators/app/index.js
@@ -3,8 +3,6 @@ var Generator = require('yeoman-generator');
 
 const {isPlainObject} = require(`lodash`);
 
-const init = require(`./lib/init`);
-
 const {
   spawnSync: spawn,
   execSync: exec
@@ -15,7 +13,7 @@ const mkdir = require(`mkdirp`);
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = Generator.Base.extend({
+module.exports = Generator.extend({
   _ucFirst(value) {
     return value.charAt(0).toUpperCase() + value.slice(1);
   },
@@ -26,7 +24,9 @@ module.exports = Generator.Base.extend({
 
   _camelCase(name) {
     return name.split(`-`).map((s, i) => {
-      if (i === 0) {return s;}
+      if (i === 0) {
+        return s;
+      }
       return this._ucFirst(s);
     }).join(``);
   },
@@ -41,7 +41,9 @@ module.exports = Generator.Base.extend({
     let from = f;
     let to = f;
 
-    if (isPlainObject(f)) {({ from, to } = f);}
+    if (isPlainObject(f)) {
+      ({from, to} = f);
+    }
 
     this.fs.copyTpl(
       this.templatePath(from),
@@ -55,37 +57,141 @@ module.exports = Generator.Base.extend({
 
   _createDir(d) {
     mkdir(d, e => {
-      if (e) {console.error(e);}
+      if (e) {
+        console.error(e);
+      }
     });
+  },
+
+  initializing() {
+    this.props = {
+      license: `MIT`,
+      year: new Date().getFullYear()
+    };
   },
 
   prompting() {
     // Have Yeoman greet the user.
     this.log(yosay(
-      'Welcome to the sweet ' + chalk.red('generator-tw5-plugin') + ' generator!'
+      'Welcome to the sweet ' + chalk.red('tw5-plugin') + ' generator!'
     ));
 
-    var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
+    var prompts = [
+      {
+        type: 'input',
+        name: 'pluginName',
+        message: 'Your plugin name',
+        default: this._ucFirst(this.appname) // Default to current folder name
+      },
+      {
+        type: 'input',
+        name: 'pluginDescription',
+        message: 'Description or what is this plugin about',
+        default: 'An awesome Tiddlywiki 5 plugin.'
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: 'Who is the author (complete name)',
+        default: 'Mr Anonymous',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: `Author's name`,
+        default: 'yo@gmail.com',
+        store: true
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: `What's your Github username`,
+        store: true
+      },
+      {
+        type: `rawlist`,
+        name: `license`,
+        message: `License`,
+        default: 'MIT',
+        choices: [{
+          value: `MIT`,
+          name: `MIT`
+        }, {
+          value: `BSD-2-Clause`,
+          name: `BSD-2-Clause`
+        }, {
+          value: `BSD-3-Clause`,
+          name: `BSD-3-Clause`
+        }, {
+          value: `ISC`,
+          name: `ISC`
+        }, {
+          value: `Apache-2.0`,
+          name: `Apache-2.0`
+        }]
+      }
+    ];
 
     return this.prompt(prompts).then(function (props) {
       // To access props later use this.props.someAnswer;
-      this.props = props;
+      this.props = Object.assign(this.props, props);
+      this.props.plugin = props.pluginName.toLowerCase();
     }.bind(this));
   },
 
   writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    this.log(this.props);
+
+    const github = [
+      // `CONTRIBUTING.md`,
+      // `.github/ISSUE_TEMPLATE.md`,
+      // `.github/PULL_REQUEST_TEMPLATE.md`
+    ];
+
+    const babel = [
+      `.babelrc`
+    ];
+
+    const editor = [
+      `.editorconfig`
+    ];
+
+    const eslint = [
+      `.eslintignore`,
+      `.eslintrc.yml`
+    ];
+
+    const git = [
+      `README.md`,
+      {from: `licences/${this.props.license}`, to: `LICENSE`},
+      {from: `_gitignore`, to: `.gitignore`}
+    ];
+
+    const npm = [
+      {from: `_package.json`, to: `package.json`}
+    ];
+
+    const ci = [
+      `.travis.yml`,
+      `gulpfile.js`
+    ];
+
+    let files = [
+      ...eslint,
+      ...git,
+      // ...github,
+      ...babel,
+      ...editor,
+      ...npm,
+      ...ci
+    ];
+
+    files.forEach(f => this._copyFile(f));
   },
 
   install() {
-    this.installDependencies();
+    // this.installDependencies();
+    this.log('Wow, let\'s install stuff');
   }
 });
---
+++ generators/app/templates/README.md
@@ -0,0 +1,6 @@
+# <%= pluginName %>
+pluginName is a plugin for TiddlyWiki 5. <%= pluginDescription %>
+
+# Installation
+
+The easies installation mechanism is by importing the bundled plugin in json format. This file is uder the dist bundle directory.
